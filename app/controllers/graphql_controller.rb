23113779ff36099e92829fb0effed7577ce5ce2c
ch0b3 <osakaya555@gmail.com>
2022-05-29 20:28:47 +0900

    comment out flagment

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -11,7 +11,7 @@ class GraphqlController < ApplicationController
     context = {
       # Query context goes here, for example:
       # current_user: current_user,
-      flags: ['member']
+      # flags: ['member']
     }
     result = GraphqlTestSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
